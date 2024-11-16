f6384bb734ec261e03b8aabcea354d1c75b03d3b
engrsakib <snazmussakib01@gmail.com>
2024-11-17 04:08:49 +0600

    update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBKTT6tMNxpLYb_v29z4NBPHHJ7y0fgG8",
-  authDomain: "simple-prohects.firebaseapp.com",
-  projectId: "simple-prohects",
-  storageBucket: "simple-prohects.firebasestorage.app",
-  messagingSenderId: "249841312516",
-  appId: "1:249841312516:web:e17c2d3abe337d5da3e651",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
